b3fc529f9ac6c801423d7e5d66769e7352cd3611
Chidubem Uba <87243600+chidubemuba@users.noreply.github.com>
2024-11-29 23:46:22 -0600

    implement initial file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+// import Sidebar from './Sidebar';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
