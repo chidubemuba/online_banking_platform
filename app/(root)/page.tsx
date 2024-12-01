90991a46536d5c3a66b2d0e03f55260cd79c7b3e
Chidubem Uba <87243600+chidubemuba@users.noreply.github.com>
2024-11-30 23:15:04 -0600

    implement homepage UI specifically the welcome header, current account balance, and doughnut visualization chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import HeaderBox from '@/components/ui/HeaderBox';
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 
-const Home: React.FC = () => {
-  return (
-	<div>
-	  Home
-	</div>
-  );
+const Home = () => {
+    const loggedIn = { firstName: "Chidubem" };
+
+    return (
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        subtext="Access and manage your account and transactions efficiently."
+                        user={loggedIn?.firstName || "Guest"}
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
---
+++ components/ui/DoughnutChart.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from 'react-chartjs-2';
+
+
+// Register the necessary Chart.js components
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+    const data: { datasets: { label: string, data: number[], backgroundColor: string[] }[], labels: string[] } = {
+        datasets: [
+            {
+                label: "Banks",
+                data: [1250, 2500, 3750],
+                backgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ],
+            }
+        ],
+        labels: ["First Bank", "GTBank", "Zenith Bank"]
+    }
+    return <Doughnut 
+        data={data} 
+        options={{
+            cutout: "60%",
+            // responsive: true,
+            // maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            }
+        }}
+    />
+}
+
+export default DoughnutChart
